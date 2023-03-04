04c0040b8d9c9d3a0d7ee588f3a3e62964c90f31
Debabrata Paul <33860706+debabrata-paul@users.noreply.github.com>
2023-03-04 13:27:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send(process.env.CYCLIC_APP_ID)
+    res.send(process.env.GITHUB_AUTH_TOKEN)
 })
 app.listen(process.env.PORT || 3000)
